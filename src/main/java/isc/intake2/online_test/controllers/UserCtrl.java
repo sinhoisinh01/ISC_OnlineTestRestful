b41a1e3b954e4f7926c9fc29f1c0a91447e32f28
sinhoisinh01 <doanphucsinh@gmail.com>
2017-04-26 21:28:21 +0700

    Making UserCtrl

--- src/main/java/isc/intake2/online_test/controllers/UserCtrl.java
+++ src/main/java/isc/intake2/online_test/controllers/UserCtrl.java
@@ -1,5 +1,102 @@
 package isc.intake2.online_test.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import isc.intake2.online_test.entities.Subject;
+import isc.intake2.online_test.entities.User;
+import isc.intake2.online_test.services.UserServiceImpl;
+
+@RestController
+@RequestMapping(
+	value = "user",
+	produces="application/json"
+)
 public class UserCtrl {
 
+	@Autowired
+	UserServiceImpl userService;
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<User>> get() {
+		List<User> users = userService.findAllUsers();
+		if (users.isEmpty()) {
+			return new ResponseEntity<List<User>>(HttpStatus.NO_CONTENT);
+		}
+		return new ResponseEntity<List<User>>(users, HttpStatus.OK);
+	}
+	
+	@RequestMapping(
+		value = "/{id}",
+		method = RequestMethod.GET
+	)
+	public ResponseEntity<User> get(@PathVariable("id") long id) {
+		User user = userService.findById(id);
+		if (user == null) {
+			return new ResponseEntity<User>(HttpStatus.NO_CONTENT);
+		}
+		return new ResponseEntity<User>(user, HttpStatus.OK);
+	}
+	
+	 //-------------------Create a User--------------------------------------------------------
+    
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<Void> add(@RequestBody User user, UriComponentsBuilder ucBuilder) {
+        System.out.println("Creating User " + user.getUserName());
+  
+        /*if (userService.isUserExist(user)) {
+            System.out.println("A User with name " + user.getName() + " already exist");
+            return new ResponseEntity<Void>(HttpStatus.CONFLICT);
+        }*/
+  
+        userService.saveUser(user);
+  
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(ucBuilder.path("/user/{id}").buildAndExpand(user.getId()).toUri());
+        return new ResponseEntity<Void>(headers, HttpStatus.CREATED);
+    }
+  
+      
+    //------------------- Update a User --------------------------------------------------------
+      
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<User> updateUser(@PathVariable("id") long id, @RequestBody User user) {
+        System.out.println("Updating User " + id);
+          
+        User currentUser = userService.findById(id);
+          
+        if (currentUser==null) {
+            System.out.println("User with id " + id + " not found");
+            return new ResponseEntity<User>(HttpStatus.NOT_FOUND);
+        }
+          
+        userService.saveOrUpdate(user);
+        return new ResponseEntity<User>(user, HttpStatus.OK);
+    }
+  
+    //------------------- Delete a User --------------------------------------------------------
+      
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<User> deleteUser(@PathVariable("id") long id) {
+        System.out.println("Fetching & Deleting User with id " + id);
+  
+        User user = userService.findById(id);
+        if (user == null) {
+            System.out.println("Unable to delete. User with id " + id + " not found");
+            return new ResponseEntity<User>(HttpStatus.NOT_FOUND);
+        }
+  
+        userService.deleteUser(userService.findById(id));
+        return new ResponseEntity<User>(HttpStatus.NO_CONTENT);
+    }
 }
